c761a275f3131430976c0dbbc8e70bebd9c06832
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-13 16:08:39 -0300

    Remaining Budget

--- src/components/Remaining/index.jsx
+++ src/components/Remaining/index.jsx
@@ -1,8 +1,18 @@
+import { AppContext } from "../../AppContext";
+import { useContext } from "react";
 const Remaining = () =>{
+    const {expenses,budget} = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((total,item)=>{
+        return (total+=item.cost)
+    },0)
+
+    const alertType = totalExpenses>budget?'alert-danger':'alert-success';
+
     return (
-        <div className="alert alert-success">
+        <div className={`alert ${alertType}`}>
             <span>
-                Remaining: R$1200
+                Remaining: R${budget-totalExpenses}
             </span>
         </div>
     )
