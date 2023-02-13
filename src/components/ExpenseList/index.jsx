cf20019932086525b16df4c18b359d69b9f1c910
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-13 15:39:40 -0300

    Expanse

--- src/components/ExpenseList/index.jsx
+++ src/components/ExpenseList/index.jsx
@@ -1,14 +1,9 @@
+import { useContext } from "react";
+import { AppContext } from "../../context"
 import ExpenseItem from "../ExpenseItem"
 
 const ExpenseList = () => {
-
-    const expenses = [
-        { id: 1231232, name: 'Shopping', cost: 50 },
-        { id: 1231232, name: 'Holiday', cost: 50 },
-        { id: 1231232, name: 'Transportation', cost: 50 },
-        { id: 1231232, name: 'Fuel', cost: 50 },
-        { id: 1231232, name: 'Child Care', cost: 50 },
-    ]
+    const { expenses } = useContext(AppContext)
 
     return (
         <ul className="list-group">
