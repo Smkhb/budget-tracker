056538d08f0f609b9777b93b3cb7c2e93a58c372
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-13 16:11:38 -0300

    Spent

--- src/components/ExpenseTotal/index.jsx
+++ src/components/ExpenseTotal/index.jsx
@@ -1,7 +1,15 @@
+import { AppContext } from "../../AppContext"
+import { useContext } from "react";
 const ExpenseTotal = ()=>{
+    const { expenses } = useContext(AppContext)
+
+    const totalExpenses = expenses.reduce((total,item)=>{
+        return (total+=item.cost)
+    },0)
     return (
         <div className="alert alert-primary">
             <span>Spent so far:R$450</span>
+            <span>Spent so far: R${totalExpenses}</span>
         </div>
     )
 }
