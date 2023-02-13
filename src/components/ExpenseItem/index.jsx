c306897e8e686e851e05e8eb1494cd118c46ef72
SAMUEL BERNARDO <samuel.hagers.bernardo@gmail.com>
2023-02-13 16:33:11 -0300

    Fix Bug

--- src/components/ExpenseItem/index.jsx
+++ src/components/ExpenseItem/index.jsx
@@ -17,7 +17,7 @@ const ExpenseItem = (props)=>{
         <li className="list-group-item d-flex justify-content-between align-itens-center">
             {props.name}
             <div>
-                <span className="badge badge-primary badge-pill mr-3">
+                <span className="'badge badge-primary badge-pill mr-3'">
                     R${props.cost}
                 </span>
                 <TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete>
